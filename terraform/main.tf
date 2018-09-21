f3dfbf0bf33d6791f6f9fcc44c687cf1c23a6e47
Lucas Cunha <luccmir@gmail.com>
2018-09-21 16:32:38 -0300

    Add terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,20 @@
 provider "google" {
-  credentials = ""
-  project      = ""
-  region       = "europe-west1"
+  project      = "your-project-name"
+  region       = "southamerica-east1"
+}
+
+resource "google_container_cluster" "primary" {
+  name               = "workshop-k8"
+  zone               = "southamerica-east1-a"
+  initial_node_count = 1
+
+  node_config {
+    machine_type = "n1-standard-1"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
 }
